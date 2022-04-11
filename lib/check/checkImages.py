284973c25c344e9f4d98020cc54e3a1c4d9ce47a
AnjaBruls <anja@transceptor.technology>
2022-04-11 18:03:16 +0200

    add CheckImages

--- lib/check/checkImages.py
+++ lib/check/checkImages.py
@@ -1,6 +1,7 @@
 import os
 
 from .base import Base
+from .utils import format_list_to_str
 
 
 class CheckImages(Base):
@@ -8,8 +9,24 @@ class CheckImages(Base):
     interval = int(os.getenv('OSDA_CHECK_IMAGES_INTERVAL', '300'))
     type_name = 'images'
 
-    @staticmethod
+    @classmethod
     def on_item(itm: dict):
-        return {
-            'name': itm['Id']
+        resp_data = {
+            'created': itm['Created'],
+            'name': itm['Id'],
+            'parentId': itm['ParentId'],
+            'repoDigests': format_list_to_str(itm['RepoDigests']),
+            'repoTags': format_list_to_str(itm['RepoTags']),
+            'size': itm['Size'],
+            'virtualSize': itm['VirtualSize'],
         }
+
+        containers = itm['Containers']
+        shared_size = itm['SharedSize']
+
+        if containers != -1:
+            resp_data['containers'] = containers
+        if shared_size != -1:
+            resp_data['sharedSize'] = shared_size
+
+        return resp_data
